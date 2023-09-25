83f3f6875087214ca8aee5d2b2b384482acd6931
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-09-25 20:51:28 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
+import React from 'react';
+import Profile from './Profile';
+import Statistics from './Statistics';
+import user from './user.json';
+import data from './data.json';
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      {/* Render the Statistics component */}
+      <Statistics title="Upload stats" stats={statisticsData} />
     </div>
   );
-};
+}
+
+export default App;
